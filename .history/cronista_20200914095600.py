4c14dcc9fbe521001aa151f664c62723d139add5
Carlos Martel Lamas <carlos.martellamas@outlook.es>
2020-10-30 08:04:14 +0000

    Corregido un bug con las rutas y los espacios

--- .history/cronista_20200914095600.py
+++ .history/cronista_20200914095600.py
@@ -25,8 +25,8 @@ def cronista_transcribe_GUI(source_file_path: str, lang: str, block_of_transcrip
 
     filename = os.path.splitext(os.path.basename(source_file_path))
 
-    audio_source_path = "{}/{}_{}.{}".format(AUDIO_PATH, filename, DATETIME, AUDIO_EXT)
-
+    audio_source_path = AUDIO_PATH + "/" + filename + \
+        "_{}".format(DATETIME) + "." + AUDIO_EXT
 
     if not lang in ('es', 'en'):
         on_transcription_progress("Se ha seleccionado un idioma no válido")
@@ -42,7 +42,7 @@ def cronista_transcribe_GUI(source_file_path: str, lang: str, block_of_transcrip
         on_transcription_progress("La cantidad introducida no está soportada, tiene que ser un número entero.")
         exit(-1)
 
-    destination_folder = "{}/{}_{}".format(AUDIO_PATH, filename, DATETIME)
+    destination_folder = AUDIO_PATH + "/" + filename + "_{}".format(DATETIME)
     if not os.path.isdir(destination_folder):
         try:
             os.mkdir(destination_folder)
@@ -76,8 +76,8 @@ def main(argv):
 
     filename, ext = os.path.splitext(os.path.basename(source_file_path))
 
-    audio_source_path = "{}/{}_{}.{}".format(AUDIO_PATH, filename, DATETIME, AUDIO_EXT)
-    #audio_source_path = AUDIO_PATH + "/" + filename + "_{}".format(DATETIME) + "." + AUDIO_EXT
+    audio_source_path = AUDIO_PATH + "/" + filename + \
+        "_{}".format(DATETIME) + "." + AUDIO_EXT
 
     lang = input(
         "Seleccione el idioma del audio que hay que transcribir (idiomas soportados: 'es' y 'en'. Por defecto: es): ") or "es"
@@ -99,8 +99,7 @@ def main(argv):
         print("La cantidad introducida no está soportada, tiene que ser un número entero.")
         exit(-1)
 
-    destination_folder = "{}/{}_{}".format(AUDIO_PATH, filename, DATETIME)
-    #destination_folder = AUDIO_PATH + "/" + filename + "_{}".format(DATETIME)
+    destination_folder = AUDIO_PATH + "/" + filename + "_{}".format(DATETIME)
     if not os.path.isdir(destination_folder):
         try:
             os.mkdir(destination_folder)
